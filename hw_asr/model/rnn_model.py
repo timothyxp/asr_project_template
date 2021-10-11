344d3ccca3a2057a8d20da79aa04278b61c78b07
Timofey S <timothyxp@mail.ru>
2021-10-11 13:00:12 +0300

    fix

--- hw_asr/model/rnn_model.py
+++ hw_asr/model/rnn_model.py
@@ -7,12 +7,15 @@ from hw_asr.base import BaseModel
 class RNNModel(BaseModel):
     def __init__(self, n_feats, n_class, hidden_size=512, num_layers=3, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
-        self.rnn = nn.LSTM(n_feats, hidden_size, num_layers=num_layers)
+
+        self.bn1 = nn.BatchNorm1d(n_feats)
+        self.rnn = nn.LSTM(n_feats, hidden_size, num_layers=num_layers, batch_first=True)
 
         self.out = nn.Linear(in_features=hidden_size, out_features=n_class)
 
     def forward(self, spectrogram, *args, **kwargs):
-        result, (h_n, c_n) = self.rnn(spectrogram)
+        result = self.bn1(spectrogram.permute(0, 2, 1)).permute(0, 2, 1)
+        result, (h_n, c_n) = self.rnn(result)
         result = self.out(result)
         return result
 
